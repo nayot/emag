fbf7786b48e576b8dd8be66ae017f8b1e859261a
Nayot Kurukitkoson <nayot@eng.buu.ac.th>
2024-02-03 20:27:22 +0700

    - Added contour plot - Added Gauss

--- libemag/emag_initials.py
+++ libemag/emag_initials.py
@@ -1,3 +1,4 @@
 from scipy.constants import pi, epsilon_0, c, mu_0
 from scipy.linalg import norm
+from scipy.integrate import quad
 from numpy import sqrt
--- libemag/plots.py
+++ libemag/plots.py
@@ -10,7 +10,8 @@ except ImportError:
 # ฟังก์ชันสำหรับ plot กราฟเวคเตอร์ โดยรับฟังกชัน field_func ซึ่งใช้ในการคำนวณค่าของสนาม ณ จุดต่าง ๆ
 def field_plot(field_func, num_grids=20,\
                x_min=-10, x_max=10, y_min=-10, y_max=10,\
-               xlabel='x', ylabel='y', title=''):
+               xlabel='x', ylabel='y', title='',\
+               contour=False, cmap='plasma'):
     x = np.linspace(x_min, x_max, num_grids)
     y = np.linspace(y_min, y_max, num_grids)
     
@@ -20,8 +21,9 @@ def field_plot(field_func, num_grids=20,\
     
     Ex = E_field[:,:,0]
     Ey = E_field[:,:,1]
+    E = np.sqrt(Ex**2 + Ey**2)
     
-    Emax = np.std(Ex) * 0.05
+    Emax = np.std(E) * 0.05
     Ex[Ex>Emax] = Emax
     Ey[Ey>Emax] = Emax
     Ex[Ex<-Emax] = -Emax
@@ -29,7 +31,14 @@ def field_plot(field_func, num_grids=20,\
     
     ax = plt.axes()
     
+    if contour:
+        E = np.log(E)
+        Emin = E.min()
+        Emax = E.max()
+        ax.contourf(X, Y, E, levels=np.linspace(Emin, Emax, 100), cmap=cmap)
+    
     ax.quiver(X, Y, Ex, Ey)
+    # Countour Plot
     
     ax.set_xlim([x_min, x_max])
     ax.set_ylim([y_min, y_max])
